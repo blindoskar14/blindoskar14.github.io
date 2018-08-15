29412308df12adafe3a9237098735dcbd0002cef
cmh <blindoskar14@gmail.com>
2018-08-15 13:15:18 +0800

    #7

--- wysocamp18/getgun.js
+++ wysocamp18/getgun.js
@@ -16,15 +16,6 @@ awm.prob = 0
 s1897.prob = 25
 m416.prob = 35
 
-
-getGun.onmouseover = getGun.onmousedown = function () {
-    intbox.style = "box-shadow: 0 10px 20px 0 rgba(255, 255, 255, 0.5);-webkit-box-shadow: 0 10px 20px 0 rgba(255, 255, 255, 0.5);"
-}
-
-getGun.onmouseup = getGun.onmouseout = function () {
-    intbox.style = "box-shadow:0;"
-}
-
 getGun.onclick = function () {
     // getGun.style.display = 'none'
     // animate(400, function () {
